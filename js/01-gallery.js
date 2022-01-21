552234b6484e5c02ca6b530a80baf535db453179
RomanZinoviev <rzinovijev@gmail.com>
2022-01-21 07:04:32 +0200

    fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,6 +3,7 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 const galleryEl = document.querySelector(".gallery");
+const basicLightboxEl = document.querySelector(".basicLightbox");
 const galleryMarup = galleryItems.map(({ preview, original, description }) => {
   return `<div class="gallery__item">
     <a class="gallery__link" href=${original}>
@@ -19,13 +20,18 @@ const galleryMarup = galleryItems.map(({ preview, original, description }) => {
 const galleryHandler = (event) => {
   event.preventDefault(); 
   if (event.target.nodeName !== "IMG") return;
-   const originalUrl = event.target.dataset.source;
- 
+   const originalUrl = event.target.dataset.source; 
   const instance = basicLightbox.create(`
-            <img src=${originalUrl} >
+            <img src="${originalUrl}" >
     `)
-  instance.show();  
+  instance.show((event) => {
+    document.addEventListener("keydown", (event) => {
+      if (event.code !== "Escape") return;
+      instance.close()    
+})
+  });  
 };
+document.removeEventListener("keydown",()=>{});
 
 galleryEl.insertAdjacentHTML("afterbegin", galleryMarup);
 galleryEl.addEventListener("click", galleryHandler);
